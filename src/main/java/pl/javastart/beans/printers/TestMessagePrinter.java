ad21864de8f1c5e0d548ada56a7c4f17c188723f
m2004michal <63554272+m2004michal@users.noreply.github.com>
2020-09-13 17:53:09 +0200

    .

--- src/main/java/pl/javastart/beans/printers/TestMessagePrinter.java
+++ src/main/java/pl/javastart/beans/printers/TestMessagePrinter.java
@@ -2,11 +2,8 @@ package pl.javastart.beans.printers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import pl.javastart.beans.decorators.Decorator;
-import pl.javastart.beans.decorators.MessageDecorator;
 import pl.javastart.beans.producers.Message;
 import pl.javastart.beans.producers.MessageProducer;
-import pl.javastart.beans.producers.Producer;
 
 @Component
 public class TestMessagePrinter {
